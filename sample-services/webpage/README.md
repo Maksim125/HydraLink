f02e59c50dd61775173d010b014b430407f69a18
Max Yarmakovich <yamaksim@sas.upenn.edu>
2024-05-26 12:24:41 -0500

    feat: poc web page

---
+++ sample-services/webpage/README.md
@@ -0,0 +1,3 @@
+# Citrus Exchange Test
+
+App currently makes fruit rain on the screen when button pressed.
---
+++ sample-services/webpage/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { Router, Route, Link } from "svelte-routing";
+	import Home from "./Home.svelte";
+	import About from "./About.svelte";
+</script>
+
+<Router>
+	<nav class="main-nav-bar">
+		<Link to="/" class="page-link">Home</Link>
+		<Link to="/about" class="page-link">About</Link>
+	</nav>
+
+	<main>
+		<Route path="/" component={Home} />
+		<Route path="/about" component={About} />
+	</main>
+</Router>
